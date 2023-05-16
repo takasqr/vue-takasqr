3fe1f7920a76a5614875aef30362e946c32810a4
takasqr <95556025+takasqr@users.noreply.github.com>
2023-05-17 05:45:33 +0900

    v-app-small NumberSlider

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,4 @@
 import BasicText from './BasicText.vue'
 import PickerWheel from './PickerWheel.vue'
-import VAppSmall from './components/VAppSmall.vue'
-import NumberSlider from './components/NumberSlider.vue'
 
-export { BasicText, PickerWheel, VAppSmall, NumberSlider }
+export { BasicText, PickerWheel }
